733dfc5a8d1f4df80c0a62e1ed5a9d77fb759915
ceniack <development@ceniack.com>
2014-09-24 22:23:31 +0500

    confilct in filter.php

--- app/filters.php
+++ app/filters.php
@@ -33,7 +33,7 @@ App::after(function($request, $response) {
 
 Route::filter('auth.custom', function() {
     if (!Confide::user()) {
-        return Redirect::to('/');
+        return Redirect::to('/admin');
     }
 
 //        return Redirect::guest('users/login');
